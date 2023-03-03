c2628380ce860a71bcee0257bafbd4a5929b5e6a
Daniel Blain <2479176B@student.gla.ac.uk>
2023-03-03 16:01:04 +0000

    changed curly brackets lol

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,8 +3,8 @@ from quiz import views
 
 app_name = 'quiz'
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path('makequiz/', views.make_quiz, name='make_quiz'),
     path('quizzes/', views.show_quizzes, name = 'show_quizzes')
-}
+]
