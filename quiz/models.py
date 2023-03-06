89acc93a9d33d7be798a320a6804562d36872834
Jason Bakes <2675743b@student.gla.ac.uk>
2023-03-06 14:13:18 +0000

    Added models based off the ER diagram no test data yet but admin interface works now

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.user.username
+
+class Quiz(models.Model):
+    title = models.CharField(max_length = 128)
+    creator = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    description = models.CharField(max_length = 255)
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    description = models.CharField(max_length = 255)
+    
+    def __str__(self):
+        return self.description
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    description = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.description
+
+class Review(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    comments = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.comments
+
+class Outcome(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    imagePath = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
